2bbd46610cdbe5e95d7b25059fc1d851252b8a65
Walter Davies <walter.davies@darkmatter.ae>
2019-03-07 13:07:11 -0700

    Personal init: turned off Guru Mode (so sorry) and added line scrolling

--- personal/wgd-init.el
+++ personal/wgd-init.el
@@ -10,3 +10,14 @@
 (desktop-save-mode 1)
 (setq desktop-restore-frames t)
 (require 'magit-todos)
+
+;; Sorry, I'm just not cool enough for Guru Mode :P
+(setq prelude-guru nil)
+
+;; But I do want to be able to "scroll-in-place"
+;; c.f: http://pragmaticemacs.com/emacs/scrolling-and-moving-by-line/
+;;keep cursor at same position when scrolling
+(setq scroll-preserve-screen-position 1)
+;;scroll window up/down by one line
+(global-set-key (kbd "M-n") (kbd "C-u 1 C-v"))
+(global-set-key (kbd "M-p") (kbd "C-u 1 M-v"))
